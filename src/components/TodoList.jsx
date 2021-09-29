43e5631967f8fbaac036da6db214c6fa800ccc74
ShindekiSan <abelchuk.maksim@gmail.com>
2021-09-29 21:51:11 +0300

    final code

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos);
+    }
+
+    const deleteTodo = id => {
+        const removeItem = todos.filter((todo) => {
+            return todo.id !== id;
+        });
+
+        setTodos(removeItem);
+    }
+
+    const changeTodoState = id => {
+        const checkTodo = todos.map((todo) => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(checkTodo)
+    }
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    }
+
+    return (
+        <div className='todo-list'>
+            <TodoForm onSubmit={addTodo} />
+            {todos.slice(0).reverse().map((todo, index) =>
+                <Todo 
+                    todo={todo} 
+                    key={index} 
+                    deleteTodo={() => deleteTodo(todo.id)} 
+                    changeTodoState={() => changeTodoState(todo.id)}
+                    updateTodo = {updateTodo}
+                />
+            )}
+        </div>
+    )
+}
+
+export default TodoList
